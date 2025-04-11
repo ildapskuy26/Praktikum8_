49c0875065fdbb3d36016979b166f003c5d4df47
ildapskuy26 <adiahmaddd398@gmail.com>
2025-04-11 09:50:19 +0700

    praktikum8

---
+++ laravel-layout/resources/views/layouts/admin.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.main')
+
+@section('title', 'Dashboard Admin')
+
+@section('header')
+    Dashboard Admin
+@endsection
+
+@section('content')
+    <p>Selamat datang di halaman admin.</p>
+@endsection
